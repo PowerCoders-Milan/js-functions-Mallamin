6ceb6b37ad88fbdf3126e587a6dd4c7ce9c9f38f
Mohamed Lamin Jalloh <mallamin@yahoo.com>
2021-06-16 15:29:01 +0200

    celsius_fahrenheit.j not dones

--- 03-celsius_fahrenheit/celsius_fahrenheit.js
+++ 03-celsius_fahrenheit/celsius_fahrenheit.js
@@ -1,12 +1,22 @@
 // define a function multiplyByNineFifths(number)
 
+const multiplyByNineFifths=(number) =>{
     //return number*(9/5)
+    return number*(9/5)
+}
+
+    
 
 // define a function getFahrenheit(celsius)
+const getFahrenheit=(celsius)=>{
+  // return   multiplyByNineFifths(celsius) + 32;
+    return multiplyByNineFifths(celsius) + 32;
+}
 
-    // return multiplyByNineFifths(celsius) + 32;
+    
 
-console.log() // call getFahrenheit();
+console.log(getFahrenheit(10)) // call getFahrenheit();
 
 
 // do the same thing starting from Fahrenheit to Celsius
+
