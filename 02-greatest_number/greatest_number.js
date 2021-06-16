e333bdb09113f990ad1934ca3f011f57c8ddb172
Mohamed Lamin Jalloh <mallamin@yahoo.com>
2021-06-16 13:39:12 +0200

    greatest_numbers done

--- 02-greatest_number/greatest_number.js
+++ 02-greatest_number/greatest_number.js
@@ -1,9 +1,31 @@
 // define an arrow function that return the maximum between 3 random numbers
+const max=(x,y,z)=>{
+      // use a conditional statement inside the function to understand wich number is the greatest one
+if(x>y && x>z){
+        return (x);    
+    }
+     
+    else if (y>x && y>z){
+        return y
+    }
+      
+        
+        else{
+            return z
+    
+    }
+}
 
-    // use a conditional statement inside the function to understand wich number is the greatest one
 
 
 // declare 3 variables and assign random numbers to them
+let x= Math.ceil(Math.random()*10)
+let y=Math.ceil(Math.random()*10)
+let z=Math.ceil(Math.random()*10)
 
 // call the function passing the numbers as arguments
-console.log(`${} is the greatest number`) //call the function inside the ${...}
+//console.log(`${} is the greatest number`) //call the function inside the ${...}
+console.log(x)
+console.log(y)
+console.log(z)
+console.log(`${max(x, y, z)} is the greatest`);
